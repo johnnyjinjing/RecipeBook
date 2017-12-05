862998a345d9a5ff09605a784b14c4af59fc1496
Jing Jin <johnnyjinjing@gmail.com>
2017-12-05 09:26:29 -0800

    add recipe and shopping list functionalities

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeSelected = new EventEmitter<Recipe>();
     recipes: Recipe[] = [
         new Recipe(
             'Test Recipe',
@@ -19,4 +20,8 @@ export class RecipeListComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {}
+
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeSelected.emit(recipe);
+    }
 }
